e58a8d0ad50dbe763d72ddbf2ded9ad9fc2f6edd
Diego Edmilson Pamo Castro <diego.pamo@tecsup.edu.pe>
2021-05-18 11:15:40 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Diego Edmilson on 5/18/21.
+//  Copyright © 2021 Diego Edmilson Pamo Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
